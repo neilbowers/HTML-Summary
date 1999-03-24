5646084863c1a843073b19a46562d88b2dbbad5a
Ave Wrigley <Ave.Wrigley@itn.co.uk>
1999-03-24 05:49:28 -0800

    import HTML-Summary 0.014 from CPAN
    git-cpan-module:   HTML-Summary
    git-cpan-version:  0.014
    git-cpan-authorid: AWRIGLEY
    git-cpan-file:     authors/id/A/AW/AWRIGLEY/HTML-Summary-0.014.tar.gz

--- test.pl
+++ test.pl
@@ -6,6 +6,24 @@
 # Change 1..1 below to 1..last_test_to_print .
 # (It may become useful if the test is moved to ./t subdirectory.)
 
+require Config;
+
+sub pathsep () {
+    $os = $^O || $Config::Config{ 'osname' } || 'UNIX';
+    if ( $os =~ /(?:win|vms|os2)/i ) 
+    {
+	return '\\';
+    } 
+    elsif ( $os =~ /mac/i ) 
+    {
+	return ':';
+    } 
+    else 
+    {
+	return '/';
+    }
+}
+
 sub cat( $ ) {
     local $/ = undef;
     open( FH, shift ) or return '';
@@ -14,7 +32,7 @@ sub cat( $ ) {
     return $text;
 }
 
-sub ok {
+sub ok( $ ) {
     my $status = shift;
     print "not " unless $status;
     print "ok $ntests\n";
@@ -25,7 +43,9 @@ BEGIN {
     $| = 1; 
     print "1..24\n"; 
 }
+
 END {print "not ok 1\n" unless $loaded;}
+
 my $ntests = 1;
 use HTML::Summary;
 use HTML::TreeBuilder;
@@ -35,14 +55,13 @@ ok( 1 );
 
 for my $file ( qw( halloween euc jis sjis ) )
 {
-    print STDERR "Creating abstract from etc/$file.html ...\n";
+    my $path = join( pathsep, 'etc', "$file.html" );
+    print STDERR "Creating abstract from $path ...\n";
     for my $length ( 50, 100, 150, 200, 250, 300 )
     {
-        print STDERR 
-            "Creating abstract of length $length from etc/$file.html ...\n"
-        ;
+        print STDERR "Creating abstract of length $length from $path ...\n";
         my $tree = new HTML::TreeBuilder;
-        $tree->parse( cat "etc/$file.html" );
+        $tree->parse( cat $path );
         my $summary = ( 
             new HTML::Summary USE_META => 0, LENGTH => $length
         )->generate( $tree );
@@ -50,10 +69,3 @@ for my $file ( qw( halloween euc jis sjis ) )
         ok( length( $summary ) <= $length );
     }
 }
-
-######################### End of black magic.
-
-# Insert your test code below (better if it prints "ok 13"
-# (correspondingly "not ok 13") depending on the success of chunk 13
-# of the test code):
-
